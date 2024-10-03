fc3a9223eaabf1e558bd36118008452c8e7ec4a5
pruiz5514 <pablo_ruiza@hotmail.com>
2024-10-03 08:39:01 -0500

    fix problems

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,14 +1,20 @@
 "use client"
+import LogoutButton from "@/components/UI/LogoutButton/LogoutButton"
+import { useSession } from "next-auth/react";
 
 const Dashboard = ()=>{
-    // const { data: session, status } = useSession();
-    // if (status === "loading") {
-    //     return <p>Loading...</p>;
-    // }
+    const { data: session, status } = useSession();
+    if (status === "loading") {
+        return <p>Loading...</p>;
+    }
     return(
         
         <>
             <div>oe</div>
+            <pre>
+                <code>{JSON.stringify(session, null, 2)}</code>
+            </pre>
+            <LogoutButton/>
         </>
     )
 }
